a628bee85cfee4a573f6d65b261c6c3c8bdd59c2
Aaron Roney <twitchax@gmail.com>
2018-02-20 23:27:11 -0800

    First commit.

---
+++ NetworkDrive/DrivePage.xaml.cs
@@ -0,0 +1,276 @@
+﻿using Microsoft.Win32;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NetworkDrive
+{
+    /// <summary>
+    /// Interaction logic for Drive.xaml
+    /// </summary>
+    public partial class DrivePage : Page
+    {
+        public static readonly DependencyProperty StatusTextProperty = DependencyProperty.Register("StatusText", typeof(string), typeof(DrivePage));
+        public string StatusText
+        {
+            get { return (string)GetValue(StatusTextProperty); }
+            set { SetValue(StatusTextProperty, value); }
+        }
+
+        public DrivePage()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        protected override async void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+            await UpdateView();
+        }
+
+        #region Blob operations.
+
+        /// <summary>
+        /// Handle double-click to download blob.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void BlockBlob_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var item = sender as TreeViewItem;
+            var blob = item.Tag as CloudBlockBlob;
+            var name = item.Header as string;
+
+            var saveFileDialog = new SaveFileDialog
+            {
+                FileName = name,
+                Title = "Download..."
+            };
+
+            if (saveFileDialog.ShowDialog() != true)
+                return;
+
+            StatusText = "Downloading...";
+            await blob.DownloadToFileAsync(saveFileDialog.FileName, FileMode.Create);
+            StatusText = "Success!";
+        }
+
+        /// <summary>
+        /// Handle double-click to upload into a directory.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void Directory_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var item = sender as TreeViewItem;
+
+            var (containerName, directoryName) = GetContainerAndDirectory(item);
+
+            var client = Helpers.Storage.CreateCloudBlobClient();
+            var container = client.GetContainerReference(containerName);
+
+            var openFileDialog = new OpenFileDialog
+            {
+                Title = "Upload..."
+            };
+
+            if (openFileDialog.ShowDialog() != true)
+                return;
+
+            var filePath = openFileDialog.FileName;
+            var fileName = filePath.Split(System.IO.Path.DirectorySeparatorChar).Last();
+
+            var blobReference = container.GetBlockBlobReference($"{directoryName}{fileName}");
+
+            StatusText = "Uploading...";
+            await blobReference.UploadFromFileAsync(filePath);
+            await UpdateView();
+            StatusText = "Success!";
+        }
+
+        /// <summary>
+        /// Handle DELETE to delete a blob.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void BlockBlob_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Delete)
+                return;
+
+            var item = sender as TreeViewItem;
+            var blob = item.Tag as CloudBlockBlob;
+            var name = item.Header as string;
+
+            StatusText = "Deleting...";
+            await blob.DeleteAsync();
+            await UpdateView();
+            StatusText = "Success!";
+        }
+
+        #endregion
+
+        #region TreeView Helpers.
+
+        /// <summary>
+        /// Refreshes the TreeView.
+        /// </summary>
+        private async Task UpdateView()
+        {
+            StatusText = "Loading...";
+
+            await Dispatcher.InvokeAsync(() =>
+            {
+                var client = Helpers.Storage.CreateCloudBlobClient();
+
+                View.Items.Clear();
+                foreach (var container in client.ListContainers())
+                    View.Items.Add(CreateRootTreeViewItem(container));
+            });
+
+            StatusText = "Success!";
+        }
+
+        /// <summary>
+        /// Create a root TreeViewItem for a Storage Container.
+        /// </summary>
+        /// <param name="container"></param>
+        /// <returns>The root TreeViewItem for the container.</returns>
+        private TreeViewItem CreateRootTreeViewItem(CloudBlobContainer container)
+        {
+            var item = new TreeViewItem
+            {
+                Header = container.Name,
+                Name = container.Name
+            };
+            item.MouseDoubleClick += Directory_MouseDoubleClick;
+
+            var blobs = container.ListBlobs(useFlatBlobListing: true)
+                .Cast<CloudBlockBlob>()
+                .Select(b => new TreeViewBlob
+                {
+                    Name = b.Name, 
+                    Blob = b
+                });
+
+            var nodes = BuildTree(blobs);
+
+            BuildTreeView(item, nodes);
+
+            return item;
+        }
+
+        /// <summary>
+        /// Build a tree out of a flat list of blobs.
+        /// </summary>
+        /// <param name="blobs"></param>
+        /// <returns>Top-levle nodes in the tree.</returns>
+        private IEnumerable<TreeViewNode> BuildTree(IEnumerable<TreeViewBlob> blobs)
+        {
+            return blobs
+                .GroupBy(b => b.Name.Split('/')[0])
+                .Select(g =>
+                {
+                    var children = g.Where(b => b.Name.Length > g.Key.Length + 1).Select(b => new TreeViewBlob
+                    {
+                        Name = b.Name.Substring(g.Key.Length + 1),
+                        Blob = b.Blob
+                    });
+
+                    var blob = g.FirstOrDefault(b => b.Name == g.Key)?.Blob;
+
+                    return new TreeViewNode
+                    {
+                        Name = g.Key,
+                        Blob = blob, 
+                        Children = BuildTree(children)
+                    };
+                });
+        }
+
+        /// <summary>
+        /// Build the TreeViewItems from the tree.
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="nodes"></param>
+        private void BuildTreeView(TreeViewItem root, IEnumerable<TreeViewNode> nodes)
+        {
+            foreach(var node in nodes)
+            {
+                var item = new TreeViewItem();
+                item.Header = node.Name;
+
+                if(node.Blob != null) // Block blob.
+                {
+                    item.Tag = node.Blob;
+                    item.MouseDoubleClick += BlockBlob_MouseDoubleClick;
+                    item.KeyUp += BlockBlob_KeyUp;
+                }
+                else // Directory.
+                {
+                    item.MouseDoubleClick += Directory_MouseDoubleClick;
+                }
+
+                BuildTreeView(item, node.Children);
+
+                root.Items.Add(item);
+            }
+        }
+
+        /// <summary>
+        /// Get the container name and directory path for a directory TreeViewItem.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>The ContainerName and DirectoryName tuple.</returns>
+        private (string ContainerName, string DirectoryName) GetContainerAndDirectory(TreeViewItem item)
+        {
+            var directoryBuilder = new StringBuilder();
+            var container = "";
+            var current = item;
+            while (current != null)
+            {
+                if (current.Parent is TreeViewItem)
+                    directoryBuilder.Insert(0, $"{current.Header.ToString()}/", 1);
+                else
+                    container = current.Header.ToString();
+
+                current = current.Parent as TreeViewItem;
+            }
+
+            var directory = directoryBuilder.ToString();
+
+            return (container, directory);
+        }
+
+        #endregion
+    }
+
+    internal class TreeViewNode
+    {
+        public string Name { get; set; }
+        public CloudBlockBlob Blob { get; set; }
+        public IEnumerable<TreeViewNode> Children { get; set; }
+    }
+
+    internal class TreeViewBlob
+    {
+        public string Name { get; set; }
+        public CloudBlockBlob Blob { get; set; }
+    }
+}
---
+++ NetworkDrive/Helpers.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkDrive
+{
+    public static class Helpers
+    {
+        public static string StorageConnectionString { get; set; }
+
+        public static CloudStorageAccount Storage => CloudStorageAccount.Parse(StorageConnectionString);
+    }
+}
